f183518ab329550a0a08aa5c061fb8f571ea9e93
Nuhan Alam <nuhanalam@gmail.com>
2025-02-15 15:11:52 +0600

    Whatever

---
+++ LeetCode_3387.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        double maxAmount(string initialCurrency, vector<vector<string>>& pairs1, vector<double>& rates1, vector<vector<string>>& pairs2, vector<double>& rates2) {
+            vector<string> countries;
+            countries.push_back(initialCurrency);
+            for(auto change:pairs1)
+            {
+                int size = countries.size();
+                for(int i=0;i<size;i++)
+                {
+                    if(change[0])
+                }
+            }
+    
+        }
+    };
+
+
+int main()
+
+{
+
+ return 0;
+
+}
+
